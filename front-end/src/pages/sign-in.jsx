d4494e7e4c59b061fb554339f2400fe27a867a41
Bises-s <101961767+Bises-s@users.noreply.github.com>
2022-12-24 15:46:16 +0000

    Completed Sign in page

---
+++ front-end/src/pages/sign-in.jsx
@@ -0,0 +1,11 @@
+import SignInComponent from "../components/sign-in";
+
+const SignUpPage = () => {
+    return (
+        <>
+            <SignInComponent />
+        </>
+    );
+}
+
+export default SignUpPage;
